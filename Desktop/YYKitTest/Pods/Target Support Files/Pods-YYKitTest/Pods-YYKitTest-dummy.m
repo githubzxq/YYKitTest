94644b90a8f015730837efdc39673b79e4c1b523
zhangxiaoqi <zhangxiaoqi@netposa.com>
2017-01-09 17:38:49 +0800

    20170109

--- Desktop/YYKitTest/Pods/Target Support Files/Pods-YYKitTest/Pods-YYKitTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YYKitTest : NSObject
-@end
-@implementation PodsDummy_Pods_YYKitTest
-@end
--- Desktop/YYKitTest/Pods/Target Support Files/Pods-YYKitTestTests/Pods-YYKitTestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YYKitTestTests : NSObject
-@end
-@implementation PodsDummy_Pods_YYKitTestTests
-@end
